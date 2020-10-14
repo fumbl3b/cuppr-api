6cd67e29f23f193f1de99ac768698b926b575ced
Harrison Winkler <harryewinkler@thinkful.com>
2020-10-14 13:08:29 -0400

    working comments endpoint

--- src/route/reviews-router.js
+++ src/route/reviews-router.js
@@ -48,4 +48,47 @@ reviewsRouter
       .catch(next);
   });
 
+reviewsRouter
+  .route('/:review_id')
+  .all(checkReviewExists)
+  .get((req, res) => {
+    res.json(res.review);
+  });
+
+reviewsRouter
+  .route('/:review_id/comments')
+  .all(checkReviewExists)
+  .get((req, res, next) => {
+    RoutesService.getCommentsForItem(
+      req.app.get('db'),
+      'comment',
+      req.params.review_id
+    )
+    .then(comments => {
+      res.json(comments)
+    })
+    .catch(next);
+  });
+
+async function checkReviewExists(req, res, next) {
+  try {
+    const review = await RoutesService.getItemById(
+      req.app.get('db'),
+      'coffee_review',
+      req.params.review_id
+    )
+
+    if (!review)
+      return res.status(404).json({
+        error: `Review doesn't exist`
+      })
+    
+    res.review = review;
+    next();
+  } catch (error) {
+      next(error);
+  }
+}
+
+
 module.exports = reviewsRouter;
--- src/route/routes-service.js
+++ src/route/routes-service.js
@@ -1,16 +1,37 @@
 const RoutesService = {
-  getAllItems(knex, db) {
-    return knex.from(db).select('*');
+  getAllItems(knex, tableName) {
+    return knex.from(tableName).select('*');
   },
-  insertItem(knex, db, newItem) {
+  getItemById(knex, tableName, id) {
+    return knex
+      .from(tableName)
+      .select('*')
+      .where(`${tableName}.id`, id)
+      .first();
+  },
+  insertItem(knex, tableName, newItem) {
     return knex
       .insert(newItem)
-      .into(db)
+      .into(tableName)
       .returning('*')
       .then(rows => {
         return rows[0];
       });
+  },
+  getCommentsForItem(knex, tableName, id) {
+    return knex
+      .from(`${tableName} AS comm`)
+      .select(
+        'comm.id',
+        'comm.author_id',
+        'comm.review_id',
+        'comm.posted_on',
+        'comm.body'
+      ).where('comm.review_id', id);
   }
+  //updateItem(knex, db, newItem) {},
+  //deleteItem(knex, db, id) {}
 };
+
 
 module.exports = RoutesService;
