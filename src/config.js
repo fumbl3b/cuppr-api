2a2c284778a953c4760040ae0591e500b4875922
Harrison Winkler <harryewinkler@thinkful.com>
2020-10-16 12:01:58 -0400

    fixed database_url

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://cuppr_admin@localhost/cuppr',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://cuppr_admin@localhost/cuppr',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://cuppr_admin@localhost/cuppr_test',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
 };
