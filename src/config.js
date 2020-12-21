83db686ec5c03bc258bda42590da451f4cedfa61
Harrison Winkler <harryewinkler@thinkful.com>
2020-12-21 15:23:15 -0500

    config edits

--- src/config.js
+++ src/config.js
@@ -6,4 +6,5 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://cuppr_admin@localhost/cuppr',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://cuppr_admin@localhost/cuppr_test',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000'
 };
